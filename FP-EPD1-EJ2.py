25f7c68d71e8d535f30a13414603ac58eea47921
David Turrión Jurado <ideivoidi08@gmail.com>
2026-09-23 12:04:25 +0200

    Add files via upload

---
+++ FP-EPD1-EJ2.py
@@ -0,0 +1,13 @@
+print("| Gestión de Alimento |")
+
+# Preguntamos datos, alimento, stock y animales
+nombre_alimento = input("· ¿Que tipo de alimento será?: ") 
+stock_inicial = float(input("· ¿Que cantidad de alimento tendrá?: "))
+cantidad_animal = int(input("· ¿Cuantos animales habrá?: "))
+
+# Cuentas para sacar los datos del stock final
+#! Puede ser < 0, pero el programa no pide que hacer en esos casos
+consumo = (cantidad_animal * 0.25) * 7
+
+# Mostramos el informe con los datos obtenidos
+print(f"| Informe:\n→ Stock Inicial: {stock_inicial}\n→ Consumo semanal: {consumo}\n→ Stock Final: {stock_inicial - consumo}")
