d93ca5df2f9b82c2d6432d730c2c2d283e35dff8
David Turrión Jurado <ideivoidi08@gmail.com>
2026-09-23 12:37:20 +0200

    Refactor IVA calculation and add constant
    Added IVA constant for tax calculation and improved code structure.

--- FP-EPD1-EJ3.py
+++ FP-EPD1-EJ3.py
@@ -1,9 +1,12 @@
 print("| Calculador con IVA |\n")
 
+# Declaramos las constantes
+IVA = 0.21
+
 # Preguntamos datos → precio base y unidades
 precio_base = float(input("· ¿Cual es el precio base del producto?: (sin IVA) "))
 unidades = int(input("· ¿Cuantas unidades fueron compradas?: "))
 
 # Mostramos el informe con los datos obtenidos
 print(f"\n| Ticket con los datos y precios:")
-print(f"→ Subtotal (Sin IVA): {precio_base * unidades}\n→ IVA: +{(precio_base * unidades) * 0.21}\n→ Total: {(precio_base * unidades) * 1.21}")
+print(f"→ Subtotal (Sin IVA): {precio_base * unidades}\n→ IVA: +{(precio_base * unidades) * IVA}\n→ Total: {(precio_base * unidades) * (1 + IVA)}")
